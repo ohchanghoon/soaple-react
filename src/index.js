e7723fb473165db18a04c953acf79428cd6b51ed
ohchanghoon <dhckdgns3@naver.com>
2023-02-07 00:08:04 +0900

    add: try click events

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Accommodate from "./chapter_07/Accommodate";
+import ConfirmButton from "./chapter_08/ComfirmButton";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-      <Accommodate/>
+      <ConfirmButton />
     </React.StrictMode>
 );
 
